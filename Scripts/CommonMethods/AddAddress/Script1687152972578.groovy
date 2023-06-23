34c750f611d4d595305c97b263cadf639a543afe
ShubhamKognivera21 <122614295+ShubhamKognivera21@users.noreply.github.com>
2023-06-23 09:14:20 +0530

    commit

---
+++ Scripts/CommonMethods/AddAddress/Script1687152972578.groovy
@@ -0,0 +1,48 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+<?xml version="1.0" encoding="UTF-8"?>
+<TestCaseEntity>
+   <description></description>
+   <name>addAddress_fillform_opc</name>
+   <tag></tag>
+   <comment></comment>
+   <testCaseGuid>55464ffb-a4ac-49b4-9e47-2b152c9f8ea7</testCaseGuid>
+</TestCaseEntity>
+<?xml version="1.0" encoding="UTF-8"?>
+<TestCaseEntity>
+   <description></description>
+   <name>addAddress_fillform_opc</name>
+   <tag></tag>
+   <comment></comment>
+   <testCaseGuid>55464ffb-a4ac-49b4-9e47-2b152c9f8ea7</testCaseGuid>
+   <variableLinks>
+	  <testDataLinkId></testDataLinkId>
+	  <type>DEFAULT</type>
+	  <value></value>
+	  <variableId>37d51d97-e880-4223-bb58-533dc8e4c138</variableId>
+   </variableLinks>
+   <variable>
+	  <defaultValue>''</defaultValue>
+	  <description></description>
+	  <id>37d51d97-e880-4223-bb58-533dc8e4c138</id>
+	  <masked>false</masked>
+	  <name>StoreValue</name>
+   </variable>
+</TestCaseEntity>
